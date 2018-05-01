cef8c723d02f046a1f39d65a7c79a52a3842cd77
Uğur İnal <ugurinal01@hotmail.com>
2018-05-01 03:45:31 +0300

    Add files via upload

---
+++ v4 perfect/Header.h
@@ -0,0 +1,30 @@
+#include <allegro5\allegro.h>
+#include <allegro5\allegro_font.h>
+#include <allegro5\allegro_native_dialog.h>
+#include <allegro5/allegro_primitives.h>
+#include <allegro5\allegro_ttf.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+enum keys { left, right };
+enum IDS { ENEMY, PLAYER };
+
+const int NUM_floor = 5;
+
+struct floor {
+	int id;
+	float x;
+	float y;
+	bool live;
+	float speed;
+};
+
+struct ball {
+	int score;
+	float x;
+	float y;
+	float speed;
+	int radius;
+};
+
+floor floors[NUM_floor];
